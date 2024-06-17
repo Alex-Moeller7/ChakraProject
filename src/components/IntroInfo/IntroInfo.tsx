3f8fda7ac36908178c513911e9f961ac6ecf981c
Alex Moeller <alex_moeller771@icloud.com>
2024-06-17 14:15:25 -0500

    Changed title and added comments to IntroInfo

--- src/components/IntroInfo/IntroInfo.tsx
+++ src/components/IntroInfo/IntroInfo.tsx
@@ -14,17 +14,29 @@ import {
   Icon,
   useColorModeValue,
 } from "@chakra-ui/react";
+
+//Imports from some outside react libraries containing various images/icons
 import { IoAnalyticsSharp, IoLogoBitcoin, IoSearchSharp } from "react-icons/io5";
 import { ReactElement } from "react";
 
+/**
+ * Props for the feature interface.
+ * Note that text and iconBg are mandatory, but Icon is not)
+ * Ex: icon? means icon prop is not mandatory, but "text" is mandatory since it has no ? at the end
+ */
 interface FeatureProps {
   text: string;
   iconBg: string;
   icon?: ReactElement;
 }
 
+/**
+ * Create custom feature tag using the above feature interface props
+ * @returns
+ */
 const Feature = ({ text, icon, iconBg }: FeatureProps) => {
   return (
+    // Simple horizontal stack of an icon and text
     <Stack direction={"row"} align={"center"}>
       <Flex w={8} h={8} align={"center"} justify={"center"} rounded={"full"} bg={iconBg}>
         {icon}
@@ -37,7 +49,9 @@ const Feature = ({ text, icon, iconBg }: FeatureProps) => {
 export default function IntroInfo() {
   return (
     <Container maxW={"5xl"} py={12}>
+      {/* SimpleGrid makes a grid layout of base rows by md rows. If you only have one object (Ex: columns={2}), it will have 2 columns and then as many rows as needed*/}
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+        {/* Stack for the initial heading and subtext, as well as contains another stack of the three key points (Discover. Explore, Build)*/}
         <Stack spacing={4}>
           <Text
             textTransform={"uppercase"}
@@ -55,6 +69,7 @@ export default function IntroInfo() {
           <Text color={"gray.500"} fontSize={"lg"}>
             The one-stop-shop for all your fragrance collection needs.
           </Text>
+          {/* Stack of three custom tag we made above */}
           <Stack
             spacing={4}
             divider={<StackDivider borderColor={useColorModeValue("gray.100", "gray.700")} />}
@@ -76,6 +91,7 @@ export default function IntroInfo() {
             />
           </Stack>
         </Stack>
+        {/* Flex box to contain the image on the right side of the page */}
         <Flex>
           <Image
             rounded={"md"}
